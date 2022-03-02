2f81b81d65e6cad4dd2906c3469408111ee50a39
i-doshechnikow <iliya.yurievich@yandex.ru>
2022-03-02 21:57:23 +0300

    add new component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
+import FeedbackItem from "./components/FeedbackItem";
 import Header from "./components/Header";
 
 const App = () => {
   return (
     <>
-      <Header text='feedback ui'/>
+      <Header text="feedback ui" />
       <div className="container">
         <h1>App</h1>
+        <FeedbackItem />
       </div>
     </>
   );
