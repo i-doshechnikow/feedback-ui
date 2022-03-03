7d048bbf4f026c6820d57f3a7998aef1b1607d33
i-doshechnikow <iliya.yurievich@yandex.ru>
2022-03-03 22:56:41 +0300

    add useState to feedbackitem

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,8 +1,21 @@
+import { useState } from "react";
+
 function FeedbackItem() {
+  const [rating, setRating] = useState(0);
+  const [text, setText] = useState("test text");
+
+  const handleClick = () => {
+    setRating((prev) => {
+      console.log(prev);
+      return rating + 1;
+    });
+  };
+
   return (
     <div className="card">
-      <div className="num-display">10</div>
-      <div className="text-display">This is an example</div>
+      <div className="num-display">{rating}</div>
+      <div className="text-display">{text}</div>
+      <button onClick={handleClick}>Click</button>
     </div>
   );
 }
