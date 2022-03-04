a4dd8e9b4476b137e44ce4ca28c8cc8bc25f445e
i-doshechnikow <iliya.yurievich@yandex.ru>
2022-03-04 21:12:08 +0300

    add data file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import { useState } from "react";
 import FeedbackItem from "./components/FeedbackItem";
 import Header from "./components/Header";
 
 const App = () => {
+  const [feedback, setFeedback] = useState([
+    {
+      id: 1,
+      rating: 8,
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia odio suscipit adipisci est quidem ea delectus praesentium similique aperiam facere?",
+    },
+    {
+      id: 2,
+      rating: 7,
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia odio suscipit adipisci est quidem ea delectus praesentium similique aperiam facere?",
+    },
+    {
+      id: 3,
+      rating: 7,
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia odio suscipit adipisci est quidem ea delectus praesentium similique aperiam facere?",
+    },
+  ]);
   return (
     <>
       <Header text="feedback ui" />
