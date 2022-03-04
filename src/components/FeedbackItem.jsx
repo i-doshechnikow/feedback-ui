4aab3e5af412af359b62a22442e4876ec332b31a
i-doshechnikow <iliya.yurievich@yandex.ru>
2022-03-04 21:04:50 +0300

    delete handle

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -4,18 +4,10 @@ function FeedbackItem() {
   const [rating, setRating] = useState(0);
   const [text, setText] = useState("test text");
 
-  const handleClick = () => {
-    setRating((prev) => {
-      console.log(prev);
-      return rating + 1;
-    });
-  };
-
   return (
     <div className="card">
       <div className="num-display">{rating}</div>
       <div className="text-display">{text}</div>
-      <button onClick={handleClick}>Click</button>
     </div>
   );
 }
