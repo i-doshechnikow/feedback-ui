f318d71ecbebc2e7561341c065f2a33cbe1c0b06
i-doshechnikow <iliya.yurievich@yandex.ru>
2022-03-05 20:33:34 +0300

    destruct props in feedback item

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 
 function FeedbackItem(props) {
-  const { item } = props;
+  const {
+    item: { rating, text },
+  } = props;
 
   return (
     <div className="card">
-      <div className="num-display">{item.rating}</div>
-      <div className="text-display">{item.text}</div>
+      <div className="num-display">{rating}</div>
+      <div className="text-display">{text}</div>
     </div>
   );
 }
