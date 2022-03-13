c54c744764ca454ca241b736dff5a4acb193cbf2
i-doshechnikow <iliya.yurievich@yandex.ru>
2022-03-13 13:21:35 +0300

    make animation

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,16 +1,31 @@
 import FeedbackItem from "./FeedbackItem";
 import PropTypes from "prop-types";
+import { motion, AnimatePresence } from "framer-motion";
 
 function FeedbackList(props) {
   const { feedback, handleDelete } = props;
   if (!feedback || feedback.length === 0) {
     return <p>No data</p>;
   }
+
   return (
     <div className="feedback-list">
+      {/* <AnimatePresence> */}
       {feedback.map((item, id) => (
-        <FeedbackItem key={id} item={item} handleDelete={handleDelete} />
+        <motion.div
+          initial={{ scale: 0 }}
+          animate={{ scale: 1 }}
+          transition={{
+            type: "spring",
+            stiffness: 260,
+            damping: 20,
+          }}
+          key={id}
+        >
+          <FeedbackItem item={item} handleDelete={handleDelete} />
+        </motion.div>
       ))}
+      {/* </AnimatePresence> */}
     </div>
   );
 }
